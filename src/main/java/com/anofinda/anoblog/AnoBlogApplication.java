357cb648168e0a2cada99751832e923e0de8f27b
nofind <2868509713@qq.com>
2023-02-24 00:02:11 +0800

    基本的配置文件

---
+++ src/main/java/com/anofinda/anoblog/AnoBlogApplication.java
@@ -0,0 +1,16 @@
+package com.anofinda.anoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dongyudeng
+ */
+@SpringBootApplication
+public class AnoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnoBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anofinda/anoblog/AnoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anofinda.anoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
