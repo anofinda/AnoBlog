9a47948e4c14c85600f47c83af2f6499294f2c02
nofind <2868509713@qq.com>
2023-02-24 16:28:10 +0800

    jwt工具类

---
+++ README.md
@@ -0,0 +1,7 @@
+### 技术栈：
+1.后端框架：spring boot  
+2.数据库：spring data jpa  
+3.权限管理：shiro整合jwt  
+4.前端框架：vue3  
+5.前端UI组件：Element Plus  
+6.markdown解析器：v-md-editor
--- src/main/java/com/anofinda/anoblog/util/JwtUtil.java
+++ src/main/java/com/anofinda/anoblog/util/JwtUtil.java
@@ -1,9 +1,16 @@
 package com.anofinda.anoblog.util;
 
 import com.anofinda.anoblog.config.JwtConfiguration;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
 
 /**
  * @author dongyudeng
@@ -13,7 +20,39 @@ import org.springframework.stereotype.Component;
 public class JwtUtil {
 
     @Autowired
-    JwtConfiguration jwtConfiguration;
+    private JwtConfiguration jwtConfiguration;
 
+    public String getToken(String username) {
+        Key key = Keys.hmacShaKeyFor(jwtConfiguration.getSecret().getBytes(StandardCharsets.UTF_8));
+        Date nowDate = new Date(System.currentTimeMillis());
+        Date expireDate = new Date(nowDate.getTime() + jwtConfiguration.getExpire() * 1000);
+        return Jwts.builder()
+                .setHeaderParam("typ", "jwt")
+                .setAudience(username)
+                .setIssuedAt(nowDate)
+                .setExpiration(expireDate)
+                .signWith(key)
+                .compact();
+    }
 
+    public Claims getClaims(String token) {
+        Key key = Keys.hmacShaKeyFor(jwtConfiguration.getSecret().getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean verify(String token, String username) {
+        Key key = Keys.hmacShaKeyFor(jwtConfiguration.getSecret().getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getAudience()
+                .equals(username);
+
+    }
 }
